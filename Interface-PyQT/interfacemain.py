c6bc408dc097ab2e9ca13b1c0f23da42eaa6d3e9
Ceejee <caro.j-rock@hotmail.com>
2015-11-02 16:31:23 -0500

    Début de la liaison entre l'interface et les scripts...path hard codés pour l'instant

--- Interface-PyQT/interfacemain.py
+++ Interface-PyQT/interfacemain.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt4 import QtGui, QtCore
+import subprocess
 
 class Window(QtGui.QMainWindow):
 
@@ -18,12 +19,24 @@ class Window(QtGui.QMainWindow):
         extractAction.setStatusTip('Fermeture')
         extractAction.triggered.connect(self.close_application)
 
+        self.styleChoice = QtGui.QLabel('Plastique', self)
+
+        win = QtGui.QAction('&Windows', self)
+        win.setStatusTip('Changement de style vers Windows')
+        win.triggered.connect(self.style_choice)
+
+        plastique = QtGui.QAction('&Plastique', self)
+        plastique.setStatusTip('Changement de style vers Plastique')
+        plastique.triggered.connect(self.style_choice)
+
         self.statusBar()
 
         mainMenu = self.menuBar()
         fileMenu = mainMenu.addMenu('&Fichier')
         fileMenu.addAction(extractAction)
-
+        styleMenu = mainMenu.addMenu('&Style')
+        styleMenu.addAction(win)
+        styleMenu.addAction(plastique)
 
 
         self.home()
@@ -45,11 +58,25 @@ class Window(QtGui.QMainWindow):
         self.btnlancer.resize(200, 100)
         self.btnlancer.clicked.connect(self.download)
 
-        self.styleChoice = QtGui.QLabel('Windows', self)
+        self.styleChoice = QtGui.QLabel('Windows Vista', self)
 
+        menuDeroulant = QtGui.QComboBox(self)
+        menuDeroulant.addItem('motif')
+        menuDeroulant.addItem('Windows')
+        menuDeroulant.addItem('cde')
+        menuDeroulant.addItem('Plastique')
+        menuDeroulant.addItem('Cleanlooks')
+        menuDeroulant.addItem('windowsvista')
+
+        menuDeroulant.move(50, 250)
+        self.styleChoice.move(50, 250)
+        menuDeroulant.activated[str].connect(self.style_choice)
 
         self.show()
 
+    def style_choice(self, text):
+        self.styleChoice.setText(text)
+        QtGui.QApplication.setStyle(QtGui.QStyleFactory.create(text))
 
     def download(self):
         self.completed = 0
@@ -57,6 +84,16 @@ class Window(QtGui.QMainWindow):
         while self.completed < 100:
             self.completed += 0.0005
             self.progress.setValue(self.completed)
+
+        self.lancerNMap()
+
+    def lancerNMap(self):
+        self.btnlancer.clicked.connect(lambda:self.run('C:\Users\Caroline\Desktop\Caro\s-curit-inm5001\s-curit-inm5001\sources\compareScan.py'))
+
+    def run(self, path):
+        scan1 = 'C:\Users\Caroline\Desktop\Caro\s-curit-inm5001\s-curit-inm5001\sources\scan-115007-102615.xml'
+        scan2 = 'C:\Users\Caroline\Desktop\Caro\s-curit-inm5001\s-curit-inm5001\sources\scan-144848-101915.xml'
+        subprocess.call(['pythonw', path, scan1, scan2])
 
     def close_application(self):
         choice = QtGui.QMessageBox.question(self, 'Quitter',
