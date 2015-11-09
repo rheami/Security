6d6ef54043ce19421c5bd8db387bd3ce82b85da1
Michel Rheault <rheaultmichel@gmail.com>
2015-11-09 01:33:50 -0500

    essayer le diff nessus

--- Interface-PyQT/test.py
+++ Interface-PyQT/test.py
@@ -9,48 +9,86 @@ import sys
 from math import *
 from PyQt4.QtCore import Qt
 from PyQt4.QtCore import pyqtSignal as Signal
-from PyQt4.QtGui import (QApplication, QDialog, QTextBrowser, QVBoxLayout, QPushButton)
+from PyQt4.QtGui import (QApplication, QDialog, QTextBrowser, QVBoxLayout, QPushButton, QHBoxLayout)
 from libnessus.parser import NessusParser
-
+from libnessus.objects.dictdiffer import DictDiffer
 
 class Form(QDialog):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
         self.nessusfile = "./xp_27.nessus"
-        self.nessus_rapport = NessusParser.parse_fromfile(self.nessusfile)
+        self.nessusfileB = "./xp_27.nessus"
+        self.nessus_rapportA = NessusParser.parse_fromfile(self.nessusfile)
+        self.nessus_rapportB = NessusParser.parse_fromfile(self.nessusfileB)
+
         self.create_widgets()
         self.layout_widgets()
         self.create_connections()
+        self.setFixedHeight(600)
+        self.setFixedWidth(800)
         self.setWindowTitle("Vunerabilite trouvees par Nessus")
 
     def create_widgets(self):
         self.browser = QTextBrowser()
-        self.nessusButton = QPushButton("Parse &Nessus file")
+        self.buttonA = QPushButton("show A")
+        self.buttonB = QPushButton("show B")
+        self.buttonDiff = QPushButton("Show diff")
 
     def layout_widgets(self):
+        buttonLayout = QHBoxLayout()
+        buttonLayout.addStretch()
+        buttonLayout.addWidget(self.buttonA)
+        buttonLayout.addWidget(self.buttonB)
+        buttonLayout.addWidget(self.buttonDiff)
         layout = QVBoxLayout()
         layout.addWidget(self.browser)
-        layout.addWidget(self.nessusButton)
+        layout.addLayout(buttonLayout)
         self.setLayout(layout)
 
     def create_connections(self):
-        self.nessusButton.clicked.connect(self.updateUi)
+        self.buttonA.clicked.connect(self.showA)
+        self.buttonB.clicked.connect(self.showB)
+        self.buttonDiff.clicked.connect(self.showDiff)
 
-    def updateUi(self):
+    def showDiff(self):
+        self.browser.clear()
+        a = {'a': 1, 'b': 1, 'c': 0}
+        b = {'a': 1, 'b': 2, 'd': 0}
+        self.d = DictDiffer(b, a)
+        self.diff = DictDiffer(b, a)
+        self.browser.append("added : <b>{0}</b> ".format(self.diff.added()))
+        self.browser.append("removed : <b>{0}</b> ".format(self.diff.removed()))
+        self.browser.append("changed : <b>{0}</b> ".format(self.diff.changed()))
+
+
+    def showB(self):
+        self.browser.clear()
         # for host in nrp.hosts: # si plusieurs hosts
-        host = self.nessus_rapport.hosts[0]
+        host = self.nessus_rapportB.hosts[0]
         vulnList = host.get_report_items
         for report_item in vulnList:
             self.browser.append("port {0} : <b>{1}</b> <b> service {2}</b><b>severity {3}</b> ".format(report_item.port, report_item.protocol, report_item.service, report_item.severity))
             if report_item.severity != "0":
                 self.browser.append("{0} = <b>{1}</b>".format("risk", report_item.get_vuln_risk))
 
-"""         afficher les details sur selection de une vulnerabilite seulement et dans une autre vue
+    def showA(self):
+        self.browser.clear()
+        # for host in nrp.hosts: # si plusieurs hosts
+        host = self.nessus_rapportA.hosts[0]
+        vulnList = host.get_report_items
+        for report_item in vulnList:
+            self.browser.append("port {0} : <b>{1}</b> <b> service {2}</b><b>severity {3}</b> ".format(report_item.port, report_item.protocol, report_item.service, report_item.severity))
+            if report_item.severity != "0":
+                self.browser.append("{0} = <b>{1}</b>".format("risk", report_item.get_vuln_risk))
+
+"""
+        afficher les details sur selection de une vulnerabilite seulement et dans une autre vue
             self.browser.append("{0} = <b>{1}</b>".format(report_item.plugin_id, report_item.plugin_name))
             self.browser.append("{0} = <b>{1}</b>".format("xref", report_item.get_vuln_xref))
             self.browser.append("{0} = <b>{1}</b>".format("info", report_item.get_vuln_info))
             self.browser.append("{0} = <b>{1}</b>".format("description", report_item.get_vuln_description))
 """
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
