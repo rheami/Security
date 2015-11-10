569a0bea75cdd67856d78ade292345bceaa133d9
Simon Hébert <simhebert@gmail.com>
2015-11-10 02:24:13 -0500

    retait de code non-utilisé, début de la restructure de l'interfacemain en modules.

--- Interface-PyQT/interfacemain.py
+++ Interface-PyQT/interfacemain.py
@@ -4,63 +4,54 @@ import subprocess
 import compareScan2
 import pickle
 
+
 class Window(QtGui.QMainWindow):
-
-    def closeEvent(self, event):
-        event.ignore()
-        self.close_application()
-
     def __init__(self):
         super(Window, self).__init__()
         self.resize(950, 450)
         self.setWindowTitle('Detecteur de vulnerabilites')
         self.setWindowIcon(QtGui.QIcon('logo.png'))
 
-        extractAction = QtGui.QAction('&Quitter', self)
-        extractAction.setShortcut('Ctrl+Q')
-        extractAction.setStatusTip('Fermeture')
-        extractAction.triggered.connect(self.close_application)
+        self.createwidget()
+        self.layoutwidget()
+        self.createconnection()
 
-        self.styleChoice = QtGui.QLabel('Plastique', self)
-
+    def createwidget(self):
+        self.progress = QtGui.QProgressBar(self)
         self.statusBar()
 
-        mainMenu = self.menuBar()
-        fileMenu = mainMenu.addMenu('&Fichier')
-        fileMenu.addAction(extractAction)
-
-        self.home()
-
-    def home(self):
-
-        self.progress = QtGui.QProgressBar(self)
-        self.progress.setGeometry(50, 380, 200, 15)
+        self.mainmenu = self.menuBar()
 
         self.btnlancer = QtGui.QPushButton('Lancer la recherche de vulnerabilites', self)
+        self.extractAction = QtGui.QAction('&Quitter', self)
+        self.boitetexte = QtGui.QTextEdit(self)
+
+    def layoutwidget(self):
+        self.extractAction.setShortcut('Ctrl+Q')
+        self.extractAction.setStatusTip('Fermeture')
+        fileMenu = self.mainmenu.addMenu('&Fichier')
+        fileMenu.addAction(self.extractAction)
+
+        self.progress.setGeometry(50, 380, 200, 15)
+
         self.btnlancer.setToolTip('Lancer la recherche')
         self.btnlancer.move(50, 50)
         self.btnlancer.resize(200, 270)
         self.btnlancer.setStatusTip('Recherche de vulnerabilites')
-        self.btnlancer.clicked.connect(self.download)
 
-        self.style_choice('plastique')
-
-        self.boitetexte = QtGui.QTextEdit(self)
         self.boitetexte.setGeometry(QtCore.QRect(330, 50, 270, 350))
         self.boitetexte.setObjectName("Resultats NMap")
 
-        self.show()
-
-    def style_choice(self, text):
-        self.styleChoice.setText(text)
-        QtGui.QApplication.setStyle(QtGui.QStyleFactory.create(text))
+    def createconnection(self):
+        self.extractAction.triggered.connect(self.close_application)
+        self.btnlancer.clicked.connect(self.download)
 
     def download(self):
-        self.completed = 0
+        completed = 0
 
-        while self.completed < 100:
-            self.completed += 0.00015
-            self.progress.setValue(self.completed)
+        while completed < 100:
+            completed += 0.00015
+            self.progress.setValue(completed)
 
         self.lancerNMap()
 
@@ -70,11 +61,14 @@ class Window(QtGui.QMainWindow):
         text = open('resultatsnmap.txt').read()
         self.boitetexte.setPlainText(text)
 
+    def closeEvent(self, event):
+        event.ignore()
+        self.close_application()
 
     def close_application(self):
         choice = QtGui.QMessageBox.question(self, 'Quitter',
                                             'Etes-vous certain de vouloir quitter?',
-                                            QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+                                            QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
         if choice == QtGui.QMessageBox.Yes:
             print('Fin normale du programme')
             sys.exit()
@@ -83,10 +77,10 @@ class Window(QtGui.QMainWindow):
 
 
 def main():
-
     app = QtGui.QApplication(sys.argv)
-    GUI = Window()
-    sys.exit(app.exec_())
+    gui = Window()
+    gui.show()
+    app.exec_()
 
 main()
 
