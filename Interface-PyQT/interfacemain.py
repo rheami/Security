7ec8586c0d28ec053403bd637da23bd4f81c00fa
Simon Hébert <simhebert@gmail.com>
2015-11-12 12:15:00 -0500

    ajustement des bouton et ajout de description dans les #todo de l'interfacemain, remise du style plastique

--- Interface-PyQT/interfacemain.py
+++ Interface-PyQT/interfacemain.py
@@ -1,5 +1,8 @@
+#!/user/bin/env python
+# coding=utf-8
 import sys
 from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import SIGNAL, SLOT
 import subprocess
 import compareScan2
 import pickle
@@ -22,22 +25,30 @@ class Window(QtGui.QMainWindow):
 
         self.mainmenu = self.menuBar()
 
-        self.btnlancer = QtGui.QPushButton('Lancer la recherche de vulnerabilites', self)
+        self.btnlancer = QtGui.QPushButton('Lancer NMap', self)
+        self.btnnessus = QtGui.QPushButton('Lancer Nessus', self)
         self.extractAction = QtGui.QAction('&Quitter', self)
         self.boitetexte = QtGui.QTextEdit(self)
 
     def layoutwidget(self):
+        self.setStyle(QtGui.QStyleFactory.create("plastique"))
+
+        self.progress.setGeometry(50, 380, 200, 15)
+
         self.extractAction.setShortcut('Ctrl+Q')
         self.extractAction.setStatusTip('Fermeture')
         fileMenu = self.mainmenu.addMenu('&Fichier')
         fileMenu.addAction(self.extractAction)
 
-        self.progress.setGeometry(50, 380, 200, 15)
+        self.btnnessus.setToolTip('Inspection et analyse des vuln') # érabilité')
+        self.btnnessus.move(50, 100)
+        self.btnnessus.resize(100, 50)
+        self.btnnessus.setStatusTip('Inspection et analyse des vuln') # érabilité')
 
-        self.btnlancer.setToolTip('Lancer la recherche')
+        self.btnlancer.setToolTip('Inspection et analyse des ports')
         self.btnlancer.move(50, 50)
-        self.btnlancer.resize(200, 270)
-        self.btnlancer.setStatusTip('Recherche de vulnerabilites')
+        self.btnlancer.resize(100, 50)
+        self.btnlancer.setStatusTip('Inspection et analyse des ports')
 
         self.boitetexte.setGeometry(QtCore.QRect(330, 50, 270, 350))
         self.boitetexte.setObjectName("Resultats NMap")
@@ -45,6 +56,8 @@ class Window(QtGui.QMainWindow):
     def createconnection(self):
         self.extractAction.triggered.connect(self.close_application)
         self.btnlancer.clicked.connect(self.download)
+        # todo transformé les appels de fonction via des bouton en signals
+        #self.connect(self.btnnessus, SIGNAL("clicked()"), self.lancerNessus())
 
     def download(self):
         completed = 0
@@ -54,6 +67,11 @@ class Window(QtGui.QMainWindow):
             self.progress.setValue(completed)
 
         self.lancerNMap()
+        # todo selection entre nmap et nessus
+
+    def lancerNessus(self):
+        pass
+        # todo lancer le script nessus via le bouton
 
     def lancerNMap(self):
         compareScan2.main('scan-115007-102615.xml', 'scan-144848-101915.xml')
