c9588ce2640d5617a99fff9e5d1fe70fef591dd0
Michel Rheault <rheaultmichel@gmail.com>
2015-11-12 17:58:10 -0500

    suite formatage du resultat de diff nessus

--- nessus/interfaceTest.py
+++ nessus/interfaceTest.py
@@ -10,14 +10,19 @@ from math import *
 from PyQt4.QtCore import Qt
 from PyQt4.QtCore import pyqtSignal as Signal
 from PyQt4.QtGui import (QApplication, QDialog, QTextBrowser, QVBoxLayout, QPushButton, QHBoxLayout)
-from parseNessus import Nessus
+from libnessus.parser import NessusParser
+from libnessus.objects.dictdiffer import DictDiffer
+
+# todo not usable now : try to adapt to new parseNessus.py when fininished
 
 class Form(QDialog):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
-        self.firstscan = "./xp_27.nessus"
-        self.secondscan = "./xp_27B.nessus"
-        self.ne = Nessus(self.firstscan, self.secondscan)
+        self.nessusfile = "./scan/xp_27.nessus"
+        self.nessusfileB = "./scan/xp_27B.nessus"
+        self.nessus_rapportA = NessusParser.parse_fromfile(self.nessusfile)
+        self.nessus_rapportB = NessusParser.parse_fromfile(self.nessusfileB)
+
         self.create_widgets()
         self.layout_widgets()
         self.create_connections()
@@ -47,14 +52,28 @@ class Form(QDialog):
     def create_connections(self):
         self.buttonA.clicked.connect(self.showA)
         self.buttonB.clicked.connect(self.showB)
+        self.buttonDiff.clicked.connect(self.showDiffDict)
         self.buttonDiffHost.clicked.connect(self.showDiffHost)
+
+    def showDiffDict(self):
+        self.browser.clear()
+        a = {'a': 1, 'b': 1, 'c': 0}
+        b = {'a': 1, 'b': 2, 'd': 0}
+        diff = DictDiffer(b, a)
+        self.browser.append("added : <b>{0}</b> ".format(diff.added()))
+        self.browser.append("removed : <b>{0}</b> ".format(diff.removed()))
+        self.browser.append("changed : <b>{0}</b> ".format(diff.changed()))
 
     def showDiffHost(self):
         self.browser.clear()
-        diffHost = self.ne.get_diff()
-        self.browser.append("added : {0}".format(diffHost['added']))
-        self.browser.append("removed : {0}".format(diffHost['removed']))
-        self.browser.append("changed : {0}".format(diffHost['changed']))
+        hosta = self.nessus_rapportA.hosts[0]
+        hostb = self.nessus_rapportB.hosts[0]
+        diffHost = hostb.diff(hosta)
+        self.browser.append("removed : <b>%s</b> " % str(diffHost['removed'].pop()))
+
+        self.browser.append("added : <h1>{0}</h1> ".format(diffHost['added']))
+        self.browser.append("removed : <b>{0}</b> ".format(diffHost['removed']))
+        self.browser.append("changed : <b>{0}</b> ".format(diffHost['changed']))
 
     def showB(self):
         self.browser.clear()
@@ -75,6 +94,14 @@ class Form(QDialog):
             self.browser.append("port {0} : <b>{1}</b> <b> service {2} </b><b>severity {3}</b> ".format(vuln.port, vuln.protocol, vuln.service, vuln.severity))
             if vuln.severity != "0":
                 self.browser.append("{0} = <b>{1}</b>".format("risk", vuln.get_vuln_risk))
+
+"""
+        afficher les details sur selection de une vulnerabilite seulement et dans une autre vue
+            self.browser.append("{0} = <b>{1}</b>".format(report_item.plugin_id, report_item.plugin_name))
+            self.browser.append("{0} = <b>{1}</b>".format("xref", report_item.get_vuln_xref))
+            self.browser.append("{0} = <b>{1}</b>".format("info", report_item.get_vuln_info))
+            self.browser.append("{0} = <b>{1}</b>".format("description", report_item.get_vuln_description))
+"""
 
 
 
--- nessus/parseNessus.py
+++ nessus/parseNessus.py
@@ -3,8 +3,7 @@
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from math import *
-from mimify import repl
+
 from libnessus.parser import NessusParser
 from libnessus.objects.dictdiffer import DictDiffer
 import os
@@ -26,12 +25,45 @@ class Nessus(object):
         return DictDiffer(b, a)
 
     def get_diff(self):
+        self.set_diff()
+        return self.diff
+
+    def set_diff(self):
         hosta = self.nessus_rapportA.hosts[0]
         hostb = self.nessus_rapportB.hosts[0]
-        return hostb.diff(hosta)
+        self.diff = hostb.diff(hosta)
 
-    def get_Vulns(self, host):
-        # for host in nrp.hosts: # si plusieurs hosts
+    def get_unchanged(self):
+        # if set diff
+        self.set_diff()
+        temp = self.diff['unchanged']
+        unchanged =  map(lambda x: x.strip('NessusReportItem::'), temp)
+        #print(unchanged)
+        vA = self.get_VulnsA()
+        #print(vA)
+        #uVa = {x: vA[x] for x in unchanged}
+        return unchanged
+
+    def get_added(self):
+        # if set diff
+        self.set_diff()
+        temp = self.diff['added']
+        return map(lambda x: x.strip('NessusReportItem::'), temp)
+
+    def get_removed(self):
+        # if set diff
+        self.set_diff()
+        temp = self.diff['removed']
+        return map(lambda x: x.strip('NessusReportItem::'), temp) # todo if NessusReportItem:: found then add value
+
+    def get_changed(self):
+        # if set diff
+        self.set_diff()
+        temp = self.diff['changed']
+        return map(lambda x: x.strip('NessusReportItem::'), temp)
+
+    def get_VulnsA(self):
+        host = self.getHostA()
         vulnList = host.get_report_items
         vulnMap = {}
         for vuln in vulnList:
@@ -39,10 +71,34 @@ class Nessus(object):
             vulnMap[vuln.plugin_id] = str
         return vulnMap
 
+    def get_VulnsB(self):
+        host = self.getHostA()
+        vulnList = host.get_report_items
+        vulnMap = {}
+        for vuln in vulnList:
+            str = "port {0} : protocol {1} service {2} severity {3}".format(vuln.port, vuln.protocol, vuln.service, vuln.severity)
+            vulnMap[vuln.plugin_id] = str
+        return vulnMap
+
+    def getHostA(self):
+        return self.nessus_rapportA.hosts[0]
+
+    def getHostB(self):
+        return self.nessus_rapportB.hosts[0]
+
+    def afficheremoved(self):
+        removed = self.get_removed()
+        vA = self.get_VulnsA()
+        for s in removed:
+            print(vA[s])
+
 
 EXIT_EQUAL = 0
 EXIT_DIFFERENT = 1
 EXIT_ERROR = 2
+
+
+
 
 
 def main():
@@ -50,17 +106,24 @@ def main():
     parser = argparse.ArgumentParser(
     description='This script parse .nessus file (XML)..')
     parser.add_argument('--firstscan',
-                    default="./xp_27.nessus",
+                    default="./scan/xp_27.nessus",
                     help="path to a nessusV2 xml")
     parser.add_argument('--secondscan',
-                    default="./xp_27B.nessus",
+                    default="./scan/xp_27B.nessus",
                     help="path to a nessusV2 xml")
     args = parser.parse_args()
 
     ne = Nessus(args.firstscan, args.secondscan)
-    print(ne.get_diff())
-    print(ne.get_Vulns(ne.nessus_rapportA.hosts[0]))
-    print(ne.get_Vulns(ne.nessus_rapportB.hosts[0]))
+    print("added")
+    print(ne.get_added())
+    print("removed")
+    print(ne.get_removed())
+    print("changed")
+    print(ne.get_changed())
+    print("unchanged")
+    print(ne.get_unchanged())
+    print(ne.get_VulnsA())
+    print(ne.get_VulnsB())
 
 # Catch uncaught exceptions so they can produce an exit code of 2 (EXIT_ERROR),
 # not 1 like they would by default.
