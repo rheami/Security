563bf68dc2074b01b7fa29b4f4fd1755f9e355b5
Simon Hébert <simhebert@gmail.com>
2015-11-27 00:32:28 -0500

    ajout de fichier test et script a tester les hashs

--- diffFiles/hashingMap.py
+++ diffFiles/hashingMap.py
@@ -13,22 +13,20 @@ class exehash(object):
         pass
 
 
+
+
 if __name__ == "__main__":
     dirtohash = "/home/cid/SecuriteDev/s-curit-inm5001/diffFiles/txt to hash/"
+    maindir = os.path.abspath(dirtohash)
     listedmap = []
 
-# file_directory = os.path.abspath(file_directory)
-# paths_to_hash = []
-#
-# for root, dirs, filenames in os.walk(file_directory, topdown=False):
-#     for i, dir in enumerate(dirs):
-#         for filename in filenames[i]:
-#             if filenames[:2] == '10':
-#                 paths_to_hash += [os.path.abspath('{0}/{1}/{2}'.format(root, dir, filename)]
-#
-# for path in paths_to_hash:
-#     hash = hashlib.md5(open(path, 'rb').read()).digest())
-#     print 'hash: {0} for path: {1}'.format(hash, path)
-#
+    for root, dirs, fnames in os.walk(maindir, topdown=False):
+        for i, dir in enumerate(dirs):
+            for f in fnames[i]:
+                listedmap += [os.path.abspath('{0}/{1}/{2}').format(root, dirs, fnames)]
+
+    for path in listedmap:
+        hash = hashlib.md5(open(path, 'rb').read()).digest()
+        print('hash: {0} for path: {1}'.format(hash, path))
 
 # todo transformer mon script en commentaire pour mettre en class, et print dans un txt
